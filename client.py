b5eafa5e9d741e443447f7e85b74431c9602cb6a
neil <horner.neil@gmail.com>
2021-03-07 12:53:47 +0000

    First code commit

---
+++ client.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from pathlib import Path
+import pyaudio
+import socket
+import sys
+import time
+import wave
+
+PORT = 4442
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = RATE * 1000
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((sys.argv[1], PORT))
+audio = pyaudio.PyAudio()
+
+
+test_dir = Path('/home/neil/Desktop/birdnet')
+
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)  # If you want to stream to speakers
+
+chunk_nu = 0
+play_audio = False
+try:
+    while True:
+        data = s.recv(CHUNK)
+
+        if play_audio:
+            stream.write(data)
+        else:
+            # Only write wav files if there are less than a certain amount to be processed
+            if len(list((test_dir.iterdir()))) > 5:
+                print('Skipping audio. Too many unprocessed files')
+                time.sleep(2)
+                continue
+            file_ = str(test_dir / f'bn_{chunk_nu}.wav')
+            f = wave.open(file_, 'w')
+            f.setnchannels(CHANNELS)
+            f.setframerate(RATE)
+            f.setsampwidth(audio.get_sample_size(FORMAT))
+            f.writeframes(data)
+            chunk_nu += 1
+
+except KeyboardInterrupt:
+    pass
+
+print('Shutting down')
+s.close()
+stream.close()
+audio.terminate()
---
+++ client_cb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from pathlib import Path
+import pyaudio
+import socket
+import sys
+import time
+import wave
+
+PORT = 4442
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 44100 * 50
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((sys.argv[1], PORT))
+audio = pyaudio.PyAudio()
+
+
+def callback(in_data, frame_count, time_info, status):
+    print('fc', frame_count)
+    data = s.recv(CHUNK)
+    print(data)
+    return (data, pyaudio.paContinue)
+
+# test_dir = Path('/home/neil/Desktop/birdnet')
+
+
+#
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK,
+                    stream_callback=callback)  # If you want to stream to speakers
+
+
+# Callback method
+stream.start_stream()
+while stream.is_active():
+    print('test')
+    time.sleep(0.1)
+
+stream.stop_stream()
+stream.close()
+
+audio.terminate()
